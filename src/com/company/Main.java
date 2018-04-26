aab9f992d13e63f411c9360dcb0322f6515f40e3
PredaG <gogu.preda@yahoo.ro>
2018-04-26 19:07:09 +0300

    Whol and fractional part from a double vall

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        double value = 12.56;
+        double fractional_part = value % 1;
+        double integral_part = value - fractional_part;
+        System.out.println("Original value: "+value);
+        System.out.println("Integral part: "+integral_part);
+        System.out.println("Fractional part: "+fractional_part);
+
+    }
+}
